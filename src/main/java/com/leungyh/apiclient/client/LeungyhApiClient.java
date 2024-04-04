4a9db34414a86b7965d964f3946326146254ca04
Leungyhhh <2111643216@qq.com>
2024-04-04 11:04:42 +0800

    优化代码逻辑

--- src/main/java/com/leungyh/apiclient/client/LeungyhApiClient.java
+++ src/main/java/com/leungyh/apiclient/client/LeungyhApiClient.java
@@ -9,6 +9,7 @@ import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
+import com.google.gson.Gson;
 import com.leungyh.apiclient.exception.ErrorCode;
 import com.leungyh.apiclient.exception.LeungyhApiException;
 import com.leungyh.apiclient.model.BaseRequest;
@@ -17,6 +18,7 @@ import com.leungyh.apiclient.model.enums.PathToMethodEnum;
 import com.leungyh.apiclient.model.response.LoveTalkResponse;
 import com.leungyh.apiclient.utils.SignUtils;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
@@ -59,23 +61,85 @@ public class LeungyhApiClient {
      * @return
      */
     public String getUsernameByPost(User user) {
-        String json = JSONUtil.toJsonStr(user);
-        HttpResponse httpResponse = HttpRequest.post(GATEWAY_HOST + "/api/name/user")
-                .addHeaders(getHeaderMap(json))
-                .body(json)
-                .execute();
-        String result = httpResponse.body();
-        return result;
+        return sendRequest(GATEWAY_HOST + PathToMethodEnum.name.getPath(), "POST", user, String.class);
     }
 
+    /**
+     * 土味情话
+     *
+     * @return
+     */
     public LoveTalkResponse getLoveTalk() {
-        HttpResponse httpResponse = HttpRequest.get(GATEWAY_HOST + PathToMethodEnum.loveTalk.getPath()).addHeaders(getHeaderMap("")).execute();
-        String loveTalk = httpResponse.body();
-        LoveTalkResponse result = new LoveTalkResponse();
-        result.setText(loveTalk);
-        return result;
+        return sendRequest(GATEWAY_HOST + PathToMethodEnum.loveTalk.getPath(), "GET", null, LoveTalkResponse.class);
     }
 
+    /**
+     * 发送调用url对应模拟接口的请求
+     *
+     * @param url
+     * @param method
+     * @param params
+     * @param responseType
+     * @param <T>
+     * @return
+     */
+    public <T> T sendRequest(String url, String method, Object params, Class<T> responseType) {
+        HttpRequest request;
+        Gson gson = new Gson();
+        String jsonBody = gson.toJson(params);
+        switch (method) {
+            case "GET":
+                request = HttpRequest.get(url);
+                request.form(convertObjToMap(params));
+                break;
+            case "POST":
+                request = HttpRequest.post(url);
+                request.header("Content-Type", "application/json; charset=UTF-8")
+                        .body(jsonBody);
+                break;
+            default:
+                throw new LeungyhApiException(ErrorCode.PARAMS_ERROR, "请求方式有误");
+        }
+        // 添加统一请求头
+        request.addHeaders(getHeaderMap(jsonBody));
+        // 发送请求 获得响应
+        HttpResponse response = request.execute();
+        String responseBody = response.body();
+        if (response.getStatus() != 200) {
+            throw new LeungyhApiException(ErrorCode.SYSTEM_ERROR, "请求接口失败");
+        }
+        return gson.fromJson(responseBody, responseType);
+    }
+
+    /**
+     * 将参数对象转换为 Map
+     *
+     * @param params
+     * @return
+     */
+    private Map<String, Object> convertObjToMap(Object params) {
+        if (params == null) {
+            return null;
+        }
+        HashMap<String, Object> map = new HashMap<>();
+        Field[] fields = params.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                map.put(field.getName(), field.get(params));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return map;
+    }
+
+    /**
+     * 解析用户调用模拟接口的请求 并调用对应的模拟接口
+     *
+     * @param baseRequest
+     * @return
+     */
     public Object resolveAndCall(BaseRequest baseRequest) {
         String path = baseRequest.getPath();
         String method = baseRequest.getMethod();
@@ -132,6 +196,12 @@ public class LeungyhApiClient {
         }
     }
 
+    /**
+     * 添加统一的请求头
+     *
+     * @param body
+     * @return
+     */
     private Map<String, String> getHeaderMap(String body) {
         Map<String, String> hashMap = new HashMap<>();
         hashMap.put("accessKey", accessKey);
